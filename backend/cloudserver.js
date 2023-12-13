69ae4fda63aa4c5e995f29c50862eca7521214d1
bandarr2 <bandarr2@gmail.com>
2023-12-13 21:03:11 +0300

    updating cloudserver

--- backend/cloudserver.js
+++ backend/cloudserver.js
@@ -20,7 +20,7 @@ const CurrencyType = new GraphQLObjectType({
 });
 const connectDB = async () =>{
   try {
-      const conn = await mongoose.connect("mongodb+srv://serviceaccount:serviceaccount123@currency.lcwbwcw.mongodb.net/?retryWrites=true&w=majority");
+      const conn = await mongoose.connect("mongodb+srv://XXXXXX:XXXXX?retryWrites=true&w=majority");
       console.log(`MongoDB connected: ${conn.connection.host}:${conn.connection.port}`)
 
       //TODO Insure Dollar is in DB
