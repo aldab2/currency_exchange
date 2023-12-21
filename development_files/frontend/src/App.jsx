b500213c128223bf625bd153e228cd8bdb192525
Abdullah Barnawi <aabarnawi@DESKTOP-N28GE6D>
2023-12-21 05:03:40 +0300

    refactor for grading

---
+++ development_files/frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MainComponent from './components/MainComponent';
+import "./App.css"
+
+function App() {
+  return (
+    <div className='container justify-content-center d-flex'>
+      <MainComponent></MainComponent>
+    </div>
+
+  );
+}
+
+export default App;
